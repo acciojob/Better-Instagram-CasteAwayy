b4491b25fe6100c417fedb6f1f096eaaf343a054
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-01 19:48:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your code here
+const images = document.querySelectorAll(".image");
+var source = null;
+var sourceElement = null;
+images.forEach((image) => {
+  image.addEventListener("dragstart", (e) => {
+    console.log("get the source");
+    console.log(getComputedStyle(e.target).backgroundImage);
+    source = getComputedStyle(e.target).backgroundImage;
+    sourceElement = e.target;
+  });
+
+  image.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+  image.addEventListener("drop", (e) => {
+    e.preventDefault();
+    console.log("drop");
+    sourceElement.style.backgroundImage = getComputedStyle(
+      e.target
+    ).backgroundImage;
+    e.target.style.backgroundImage = source;
+  });
+});
